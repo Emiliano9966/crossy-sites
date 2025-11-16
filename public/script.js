29cd42172a91a6772691547487447d855ba4c97c
Emiliano <76189306+Emiliano9966@users.noreply.github.com>
2025-11-16 00:29:03 -0500

    Add files via upload

---
+++ public/script.js
@@ -0,0 +1,44 @@
+const allGames = [
+    {
+        name: "Capybara Clicker",
+        image: "/img/cappyclicker.png",
+        html: "/games/cappyclicker.html"
+    },
+    {
+        name: "BitLife",
+        image: "/img/bitlife.png",
+        html: "/games/bitlife.html"
+    },
+    {
+        name: "Drift Boss",
+        image: "/img/driftboss.png",
+        html: "/games/driftboss.html"
+    }
+    // add more games here
+];
+
+// Example continue-playing (you can fill using localStorage if u want)
+const continuePlaying = [
+    allGames[0],
+    allGames[1]
+];
+
+function populateRow(rowId, games) {
+    const row = document.getElementById(rowId);
+
+    games.forEach(g => {
+        const card = document.createElement("div");
+        card.className = "card";
+
+        card.innerHTML = `
+            <img src="${g.image}">
+            <div class="card-title">${g.name}</div>
+        `;
+
+        card.onclick = () => window.location = g.html;
+        row.appendChild(card);
+    });
+}
+
+populateRow("continue-row", continuePlaying);
+populateRow("all-row", allGames);
